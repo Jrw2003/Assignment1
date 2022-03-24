bcc1e3336abe32528a12aed4c7160ab194ab0754
Joshua Woodcock <40006088@student.northlindsey.ac.uk>
2022-03-24 15:01:10 +0000

    Add project files.

---
+++ ConsoleApp2/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Input
+    {
+        //Handles the text input for Assessment 1
+        string text = "nothing";
+
+        //Method: manualTextInput
+        //Arguments: none
+        //Returns: string
+        //Gets text input from the keyboard
+        public string manualTextInput()
+        {
+            string manualText = Console.ReadLine();
+            return manualText;
+        }
+
+        //Method: fileTextInput
+        //Arguments: string (the file path)
+        //Returns: string
+        //Gets text input from a .txt file
+        public string fileTextInput(string fileName)
+        {
+            fileName = File.ReadAllText(@"C: \Users\40006088\OneDrive - DN Colleges Group\Uni Work\OOP file.txt");
+            return text;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+            Input input = new Input();
+            Console.WriteLine("Do you want to manually input a sentence or use the already created text file?(write manually or textfile)");
+            if 
+            Console.WriteLine(input.manualTextInput());
+
+
+            //Create an 'Analyse' object
+            //Pass the text input to the 'analyseText' method
+
+
+            //Receive a list of integers back
+
+
+            //Report the results of the analysis
+
+
+            //TO ADD: Get the frequency of individual letters?
+
+
+        }
+
+
+
+    }
+}
